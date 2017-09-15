33f1ad5a1f3502273e4833c170f36bd904bcf1dc
Mac Bellingrath <mbellingrath@weddingwire.com>
2017-09-15 15:39:12 -0400

    removes inspectable

--- ActiveLabel/ActiveLabel.swift
+++ ActiveLabel/ActiveLabel.swift
@@ -36,19 +36,19 @@ typealias ElementTuple = (range: NSRange, element: ActiveElement, type: ActiveTy
     @IBInspectable open var mentionColor: UIColor = .blue {
         didSet { updateTextStorage(parseText: false) }
     }
-    @IBInspectable open var mentionSelectedColor: UIColor? {
+    open var mentionSelectedColor: UIColor? {
         didSet { updateTextStorage(parseText: false) }
     }
     @IBInspectable open var hashtagColor: UIColor = .blue {
         didSet { updateTextStorage(parseText: false) }
     }
-    @IBInspectable open var hashtagSelectedColor: UIColor? {
+    open var hashtagSelectedColor: UIColor? {
         didSet { updateTextStorage(parseText: false) }
     }
     @IBInspectable open var URLColor: UIColor = .blue {
         didSet { updateTextStorage(parseText: false) }
     }
-    @IBInspectable open var URLSelectedColor: UIColor? {
+    open var URLSelectedColor: UIColor? {
         didSet { updateTextStorage(parseText: false) }
     }
     open var customColor: [ActiveType : UIColor] = [:] {
@@ -63,10 +63,10 @@ typealias ElementTuple = (range: NSRange, element: ActiveElement, type: ActiveTy
     @IBInspectable public var minimumLineHeight: CGFloat = 0 {
         didSet { updateTextStorage(parseText: false) }
     }
-    @IBInspectable public var highlightFontName: String? = nil {
+    public var highlightFontName: String? {
         didSet { updateTextStorage(parseText: false) }
     }
-    @IBInspectable public var highlightFontSize: CGFloat? = nil {
+    public var highlightFontSize: CGFloat? {
         didSet { updateTextStorage(parseText: false) }
     }
     
